cf7a1030857ce421554972e6fc9d04d7ac951773
Ubuntu <ubuntu@ip-172-31-18-99.ap-northeast-1.compute.internal>
2026-09-07 09:03:45 +0000

    [Finish] 3-8

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,12 +3,12 @@ class ListsController < ApplicationController
     @list = List.new 
   end
  def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    list.save
-   # 詳細画面へリダイレクト
-    redirect_to list_path(list.id)
+      @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
@@ -27,6 +27,12 @@ class ListsController < ApplicationController
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to list_path(list.id)  
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
   end
   
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get "lists/new"
-    post 'lists' => 'lists#create' 
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-     patch 'lists/:id' => 'lists#update', as: 'update_list'
   get '/top' => 'homes#top'
+
+  resources :lists
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
