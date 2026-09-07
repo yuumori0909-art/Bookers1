bc7b9e059f9ceae6e14a0ad26b8e2542141c1354
Ubuntu <ubuntu@ip-172-31-18-99.ap-northeast-1.compute.internal>
2026-09-07 07:08:41 +0000

    [Finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,8 +7,8 @@ class ListsController < ApplicationController
     list = List.new(list_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+   # 詳細画面へリダイレクト
+    redirect_to list_path(list.id)
   end
 
   def index
@@ -16,9 +16,17 @@ class ListsController < ApplicationController
   end
 
   def show
+      @list = List.find(params[:id])  
   end
 
   def edit
+      @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)  
   end
   
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get "lists/new"
     post 'lists' => 'lists#create' 
   get 'lists' => 'lists#index'
-  get "lists/show"
-  get "lists/edit"
+  get 'lists/:id' => 'lists#show', as: 'list'
+   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+     patch 'lists/:id' => 'lists#update', as: 'update_list'
   get '/top' => 'homes#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
